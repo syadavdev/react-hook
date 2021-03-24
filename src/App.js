5bcb34ba9d47364ab0687d6387048efa41c73064
Sandeep Yadav <sandi10.96@gmail.com>
2021-03-24 23:07:51 +0530

    learn api call by react hooks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import ClassCounterOne from './components/ClassCounterOne';
+import DataFetching from './components/DataFetching';
 import HookCounter from './components/HookCounter';
 import HookCounterFour from './components/HookCounterFour';
 import HookCounterOne from './components/HookCounterOne';
 import HookCounterThree from './components/HookCounterThree';
 import HookCounterTwo from './components/HookCounterTwo';
 import HookMouse from './components/HookMouse';
+import IntervalHookCounter from './components/IntervalHookCounter';
 import MouseContainer from './components/MouseContainer';
 
 function App() {
   return (
     <div className="App">
-      <MouseContainer />
+      <DataFetching />
+      {/* <IntervalHookCounter /> */}
+      {/* <MouseContainer /> */}
       {/* <HookMouse /> */}
       {/* <ClassCounterOne /> */}
       {/* <HookCounterOne /> */}
