cf44b3e8b7010ea4a8a966da015a136f224e953a
Sandeep Yadav <sandi10.96@gmail.com>
2021-03-25 22:48:50 +0530

    learn react hook usecontext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import React from 'react'
 import ClassCounter from './components/ClassCounter';
 import ClassCounterOne from './components/ClassCounterOne';
+import ComponentC from './components/ComponentC';
 import DataFetching from './components/DataFetching';
 import HookCounter from './components/HookCounter';
 import HookCounterFour from './components/HookCounterFour';
@@ -11,10 +13,18 @@ import HookMouse from './components/HookMouse';
 import IntervalHookCounter from './components/IntervalHookCounter';
 import MouseContainer from './components/MouseContainer';
 
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
+
 function App() {
   return (
     <div className="App">
-      <DataFetching />
+      <UserContext.Provider value={'Sandeep'}>
+        <ChannelContext.Provider value='Yadav'>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
+      {/* <DataFetching /> */}
       {/* <IntervalHookCounter /> */}
       {/* <MouseContainer /> */}
       {/* <HookMouse /> */}
