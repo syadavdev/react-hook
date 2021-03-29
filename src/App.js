bc47a9b1e5083412a105771b9731bab153226cbd
Sandeep Yadav <sandi10.96@gmail.com>
2021-03-29 23:58:43 +0530

    learn react useReducer with useContext

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react'
+import React, {useReducer} from 'react'
 import ClassCounter from './components/ClassCounter';
 import ClassCounterOne from './components/ClassCounterOne';
 import ComponentC from './components/ComponentC';
@@ -13,14 +13,37 @@ import HookMouse from './components/HookMouse';
 import IntervalHookCounter from './components/IntervalHookCounter';
 import MouseContainer from './components/MouseContainer';
 import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
+import CounterThree from './components/CounterThree';
+import ComponentA from './components/ComponentA';
+import ComponentB from './components/ComponentB';
 
 export const UserContext = React.createContext()
 export const ChannelContext = React.createContext()
+export const CountContext = React.createContext()
+
+const initialState = 0
+const reducer = (state, action) => {
+    switch(action) {
+        case 'increment' : return state + 1
+        case 'decrement' : return state - 1
+        case 'reset' : return initialState
+        default: return state
+    }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
     <div className="App">
-      <CounterOne />
+      <CountContext.Provider value={{countState: count, countDispatch: dispatch}}>
+      Count - {count}
+      <ComponentA />
+      <ComponentB />
+      </CountContext.Provider>
+      {/* <CounterThree /> */}
+      {/* <CounterTwo /> */}
+      {/* <CounterOne /> */}
       {/* <UserContext.Provider value={'Sandeep'}>
         <ChannelContext.Provider value='Yadav'>
           <ComponentC />
---
+++ src/components/CounterThree.js
@@ -0,0 +1,34 @@
+import React, {useState, useReducer} from 'react'
+
+const initialState = 0
+const reducer = (state, action) => {
+    switch(action) {
+        case 'increment' : return state + 1
+        case 'decrement' : return state - 1
+        case 'reset' : return initialState
+        default: return state
+    }
+}
+
+function CounterThree() {
+
+    const [count, dispatch] = useReducer(reducer, initialState)
+    const [countTwo, dispatchTwo] = useReducer(reducer, initialState)
+
+    return (
+        <div>
+            <button onClick={() => dispatch('increment')}>Increment</button>
+            <button onClick={() => dispatch('decrement')}>Decrement</button>
+            <button onClick={() => dispatch('reset')}>Reset</button>
+            <div>{count}</div>
+            <div>
+            <button onClick={() => dispatchTwo('increment')}>Increment</button>
+            <button onClick={() => dispatchTwo('decrement')}>Decrement</button>
+            <button onClick={() => dispatchTwo('reset')}>Reset</button>
+            <div>{countTwo}</div>
+            </div>
+        </div>
+    )
+}
+
+export default CounterThree
